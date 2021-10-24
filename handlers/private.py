2834ca14b6a329568d81da3f31801a02d9aa8f17
Tᴄ Dᴏᴘᴀ Oᴘ <88358699+Dopamusicbot@users.noreply.github.com>
2021-10-24 15:43:00 +0530

    Update private.py

--- handlers/private.py
+++ handlers/private.py
@@ -7,7 +7,7 @@ from helpers.filters import other_filters2
 
 @Client.on_message(other_filters2)
 async def start(_, message: Message):
-    await message.reply_image("https://telegra.ph/file/088455df524bc7d105c44.jpg")
+    await message.reply_sticker("CAACAgQAAx0CTv65QgABBfJlYF6VCrGMm6OJ23AxHmD6qUSWESsAAhoQAAKm8XEeD5nrjz5IJFYeBA")
     await message.reply_text(
         f"""**ʜᴇʏ, I'm {bn} 🎵
 
